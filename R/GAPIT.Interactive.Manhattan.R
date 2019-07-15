1f16a5b494239322be80bdf669d570da8ed48adf
Jiabo Wang <33234975+jiabowang@users.noreply.github.com>
2019-07-15 18:31:50 +0800

    Update GAPIT.Interactive.Manhattan.R

--- R/GAPIT.Interactive.Manhattan.R
+++ R/GAPIT.Interactive.Manhattan.R
@@ -89,9 +89,9 @@ if(c("m")%in%plot.type)
     text = ~paste("SNP: ", taxa, "<br>Posi: ", posi,"<br>MAF: ", round(maf,2),"<br>Effect: ",round(effect,2)),
     color = ~as.character(zz)
     )%>%
-  add_trace(y=bonferroniCutOff01,name = 'CutOff-0.01',color=I("red"),mode="line",width=1.4,text="")%>%
-  add_trace(y=bonferroniCutOff05,name = 'CutOff-0.05',color=I("red"),mode="line",line=list(width=1.4,dash='dot'),text="")%>%
-  layout(title = "Interactive.Manhattan.Plot",
+   add_trace(y=bonferroniCutOff01,name = 'CutOff-0.01',color=I("red"),mode="line",width=1.4,text="")%>%
+   add_trace(y=bonferroniCutOff05,name = 'CutOff-0.05',color=I("red"),mode="line",line=list(width=1.4,dash='dot'),text="")%>%
+   layout(title = "Interactive.Manhattan.Plot",
          #showticklabels = FALSE,
          #legend = list(orientation = 'h'),
          xaxis = list(title = "Chromsome",zeroline = FALSE,showticklabels = FALSE),
