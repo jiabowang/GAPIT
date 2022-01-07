8f3ab2433aeb64c12a74c0c8fe6a728f9cc19222
Jiabo Wang <33234975+jiabowang@users.noreply.github.com>
2022-01-07 08:14:59 +0800

    Update GAPIT.library.R

--- R/GAPIT.library.R
+++ R/GAPIT.library.R
@@ -1,17 +1,16 @@
+if(!require(gplots)) install.packages("gplots")
+if(!require(LDheatmap)) install.packages("LDheatmap")
+if(!require(genetics)) install.packages("genetics")
+if(!require(ape)) install.packages("ape")
+if(!require(compiler)) install.packages("compiler")
+if(!require(grid)) install.packages("grid")
 
-#if(!require(gplots)) install.packages("gplots")
-#if(!require(LDheatmap)) install.packages("LDheatmap")
-#if(!require(genetics)) install.packages("genetics")
-#if(!require(ape)) install.packages("ape")
-#if(!require(compiler)) install.packages("compiler")
-#if(!require(grid)) install.packages("grid")
+if(!require(EMMREML)) install.packages("EMMREML")
+if(!require(scatterplot3d)) install.packages("scatterplot3d")
 
-#if(!require(EMMREML)) install.packages("EMMREML")
-#if(!require(scatterplot3d)) install.packages("scatterplot3d")
-
-#if(!'multtest'%in% installed.packages()[,"Package"]){
-#	if (!requireNamespace("BiocManager", quietly = TRUE))
-#    install.packages("BiocManager")
-#    BiocManager::install("multtest")
-#    BiocManager::install("snpStats")
-#}
+if(!'multtest'%in% installed.packages()[,"Package"]){
+	if (!requireNamespace("BiocManager", quietly = TRUE))
+   install.packages("BiocManager")
+   BiocManager::install("multtest")
+   BiocManager::install("snpStats")
+}
