d54f8bbef5f64dae371dba6c4a3519ef9007ef81
jiabowang <jiabowangyifeng@gmail.com>
2022-11-01 19:36:16 +0800

    Add ld.cut in the randommodel

--- R/GAPIT.RandomModel.R
+++ R/GAPIT.RandomModel.R
@@ -1,5 +1,5 @@
 `GAPIT.RandomModel` <-
-function(GWAS,Y,CV=NULL,X,cutOff=0.01,GT=NULL,name.of.trait=NULL,N.sig=NULL,n_ran=500){
+function(GWAS,Y,CV=NULL,X,cutOff=0.01,GT=NULL,name.of.trait=NULL,N.sig=NULL,n_ran=500,ld.cut=FALSE){
     #Object: To calculate the genetics variance ratio of Candidate Genes
     #Output: The genetics variance raito between CG and total
     #Authors: Jiabo Wang and Zhiwu Zhang
@@ -7,10 +7,6 @@ function(GWAS,Y,CV=NULL,X,cutOff=0.01,GT=NULL,name.of.trait=NULL,N.sig=NULL,n_ra
     ##############################################################################################
     if(!require(lme4))  install.packages("lme4")
     library("lme4")
-    # GWAS=myGAPIT_SUPER$GWAS
-    # CV=myGAPIT_SUPER$PCA
-    # cut.set=0.01
-    #return(list(GVs=NULL))
     print("GAPIT.RandomModel beginning...")
     if(is.null(GT))GT=as.character(Y[,1])
     # name.of.trait=colnames(Y)[2]
@@ -37,9 +33,12 @@ function(GWAS,Y,CV=NULL,X,cutOff=0.01,GT=NULL,name.of.trait=NULL,N.sig=NULL,n_ra
     }
     geneGD=X[,index,drop=FALSE]
     geneGWAS=GWAS[index,,drop=FALSE]
-    gene.licols=GAPIT.Licols(X=geneGD)
-    geneGD=gene.licols$Xsub
-    geneGWAS=geneGWAS[gene.licols$idx,]
+    if(ld.cut)
+    {
+        gene.licols=GAPIT.Licols(X=geneGD)
+        geneGD=gene.licols$Xsub
+        geneGWAS=geneGWAS[gene.licols$idx,]
+    }
     index_T=as.matrix(table(index))
     # print(index_T)
     in_True=ncol(geneGD)
@@ -48,8 +47,6 @@ function(GWAS,Y,CV=NULL,X,cutOff=0.01,GT=NULL,name.of.trait=NULL,N.sig=NULL,n_ra
     {
     	colnames(geneGD)=paste("gene_",1:in_True,sep="")
     }
-
-
     colnames(Y)=c("taxa","trait")
     if(is.null(CV))
     {
@@ -59,13 +56,7 @@ function(GWAS,Y,CV=NULL,X,cutOff=0.01,GT=NULL,name.of.trait=NULL,N.sig=NULL,n_ra
     	return(list(GVs=NULL))
     	}     	
     	taxa_Y=as.character(Y[,1])
-    	# print("!!")
-     #    print(dim(geneGD))
-        # print(taxa_Y)
-        
         geneGD=geneGD[GT%in%taxa_Y,]
-        
-     # if(!is.null(PC))PC=PC[taxa_GD%in%taxa_Y,]
         Y=Y[taxa_Y%in%GT,]
         tree2=cbind(Y,geneGD)
     	# CV[,2]=1
@@ -78,15 +69,7 @@ function(GWAS,Y,CV=NULL,X,cutOff=0.01,GT=NULL,name.of.trait=NULL,N.sig=NULL,n_ra
     	    return(list(GVs=NULL))
     	    }  
     	taxa_Y=as.character(Y[,1])
-    	# print(dim(geneGD))
-    	# print(head(GT))
-    	# print(head(taxa_Y))
-         # if(in_True==1)
-         #    {
-        	# geneGD=geneGD[GT%in%taxa_Y]
-         #    }else{
-        	geneGD=geneGD[GT%in%taxa_Y,]
-            # }     # if(!is.null(PC))PC=PC[taxa_GD%in%taxa_Y,]
+        geneGD=geneGD[GT%in%taxa_Y,]
         Y=Y[taxa_Y%in%GT,]
         tree2=cbind(Y,geneGD)
     	}else{
@@ -98,47 +81,28 @@ function(GWAS,Y,CV=NULL,X,cutOff=0.01,GT=NULL,name.of.trait=NULL,N.sig=NULL,n_ra
     	colnames(CV)=c("Taxa",paste("CV",1:(ncol(CV)-1),sep=""))
     	taxa_Y=as.character(Y[,1])
     	taxa_CV=as.character(CV[,1])
-        
         geneGD=geneGD[GT%in%taxa_Y,]
-            # if(!is.null(PC))PC=PC[taxa_GD%in%taxa_Y,]
-     # if(!is.null(PC))PC=PC[taxa_GD%in%taxa_Y,]
         Y=Y[taxa_Y%in%GT,]
         CV=CV[taxa_CV%in%GT,]
     	tree2=cbind(Y,CV[,-1,drop=FALSE],geneGD) # thanks jeremyde 2022.9.14
         }
     }
     if(in_True==1)colnames(tree2)[ncol(tree2)]=paste("gene_",1,sep="")
-
-    	# print(head(tree2))
-     #    print(dim(CV))
-     #    print(dim(geneGD))
     n_cv=ncol(CV)-1
-        # print(n_cv)
     n_gd=in_True
     n_id=nrow(Y)
-    # print(n_gd)
-    # print(n_cv)
-    # print(n_id)
-    # if((n_gd+n_cv)>n_ran)
-    # {
-    # 	print("The candidate markers are more than threshold value !")
-    # 	return(list(GVs=NULL))
-    # 	} 
+    
 if(!is.null(CV))
 {
-#ff <- paste("trait~1+PC1+PC2+PC3+(1|gene_1)+(1|gene_2)+(1|gene_3)+(1|gene_4)+(1|gene_5)+(1|gene_6)"
-#dflme <- lmer(ff, data=tree2)
     if(ncol(CV)==1)
     {
-    command0=paste("trait~1",sep="")
-    command1=command0
-    
-    command2=command1
-    for(j in 1:n_gd)
-    {
-	   command2=paste(command2,"+(1|gene_",j,")",sep="")
-    }
-
+      command0=paste("trait~1",sep="")
+      command1=command0  
+      command2=command1
+      for(j in 1:n_gd)
+      {
+	     command2=paste(command2,"+(1|gene_",j,")",sep="")
+      }
     }else{
        command0=paste("trait~1",sep="")
        command1=command0
@@ -146,27 +110,22 @@ if(!is.null(CV))
        {	
 	       command1=paste(command1,"+CV",i,sep="")
        }
-           command2=command1
+       command2=command1
        for(j in 1:n_gd)
        {
     	   command2=paste(command2,"+(1|gene_",j,")",sep="")
        }
     }
 }else{
-
     command0=paste("trait~1",sep="")
-    command1=command0
-    
+    command1=command0  
     command2=command1
     for(j in 1:n_gd)
     {
     	command2=paste(command2,"+(1|gene_",j,")",sep="")
     }
-
 }
-# print(head(tree2))
-# print(apply(tree2,2,unique))
-#command3=paste(command2,"+(1|gene_",j,")",sep="")
+
     dflme <- lme4::lmer(command2, data=tree2, control = lme4::lmerControl(check.nobs.vs.nlev = "ignore",
      check.nobs.vs.rankZ = "ignore",
      check.nobs.vs.nRE="ignore"))
@@ -184,9 +143,6 @@ if(!is.null(CV))
     utils::write.csv(gene_list,paste("GAPIT.Association.PVE.", name.of.trait,".csv",sep=""),quote = FALSE, sep = ",", row.names = FALSE,col.names = TRUE)
 #gene_list=read.csv("GAPIT.Weight.GrowthIntercept.Phenotype_Variance_Explained_by_Association_Markers.csv",head=T)
     colnames(gene_list)[ncol(gene_list)]="Variance_Explained"
-# print(gene_list)
-# print(is.na(gene_list[1,c(4:8)]))
-
 if(sum(is.na(gene_list[1,c(4:8)]))==0)
 {
          grDevices::pdf(paste("GAPIT.Association.Effect_PVE.", name.of.trait,".pdf" ,sep = ""), width = 7,height=5.75)
@@ -208,12 +164,10 @@ if(sum(is.na(gene_list[1,c(4:8)]))==0)
 
     if(n_gd>=5)
         {
-        grDevices::pdf(paste("GAPIT.Association.MAF_Effect_PVE.", name.of.trait,".pdf" ,sep = ""), width = 9,height=5.75)
-        
+        grDevices::pdf(paste("GAPIT.Association.MAF_Effect_PVE.", name.of.trait,".pdf" ,sep = ""), width = 9,height=5.75)      
         n=10
         graphics::layout(matrix(c(1,1,2,1,1,1,1,1,1),3,3,byrow=TRUE), c(2,1), c(1,1), TRUE)
         do_color = grDevices::colorRampPalette(c("green", "red"))(n)
-
             graphics::par(mar=c(4,5,2,8),cex=1)
             y=as.numeric(gene_list$maf)
             x=as.numeric(gene_list$effect)
@@ -231,31 +185,16 @@ if(sum(is.na(gene_list[1,c(4:8)]))==0)
                  xlab = "",ylab = "", cex.lab=1.2,pch=21,bg=r2_color)
             graphics::mtext("Estimated Effect",side=1,line=2.5)
             graphics::mtext("MAF",side=2,line=2.5)
-
-
-            graphics::par(mar=c(2,6,3,3))
-            
+            graphics::par(mar=c(2,6,3,3))           
             graphics::barplot(matrix(rep(0.4,times=n),n,1),beside=T,col=do_color,border=do_color,axes=FALSE,horiz =T)
-        #legend(x=10,y=2,legend=expression(R^"2"),,lty=0,cex=1.3,bty="n",bg=par("bg"))
-            # print(z)
             step=length(seq(0,round(max(z),3),by=0.01))
             small_bar=round(seq(0,round(max(z),3),by=(max(z)-min(z))/10),2)
-            #main()
             graphics::mtext("Phenotypic Variance Explained (%)",side=2,line=0.4,col="black",cex=0.7)
-
-            graphics::axis(4,c(1,6,11),c(min(small_bar),stats::median(small_bar),max(small_bar)),las=2,cex.axis = 0.7,tick=F,line=0)
-        
-        grDevices::dev.off()
+            graphics::axis(4,c(1,6,11),c(min(small_bar),stats::median(small_bar),max(small_bar)),las=2,cex.axis = 0.7,tick=F,line=0) 
+            grDevices::dev.off()
         }
 }
-
-
-
-
-
-
 return(list(GVs=var_gene/sum(var_gene+var_res),PVEs=gene_list))
-
 }#end of GAPIT.RandomModel function
 #=============================================================================================
           
